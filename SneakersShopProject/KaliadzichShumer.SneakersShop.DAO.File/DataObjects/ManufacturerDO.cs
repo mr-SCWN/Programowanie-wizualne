c4be1dd7f65d3e7d71df351a51e1f6d8a901f8b1
DaniilShumer <daniil.shumer@gmail.com>
2025-01-26 19:45:05 +0100

    delete

--- SneakersShopProject/KaliadzichShumer.SneakersShop.DAO.File/DataObjects/ManufacturerDO.cs
+++
@@ -1,8 +0,0 @@
-namespace KaliadzichShumer.SneakersShop.DAO.File.DataObjects
-{
-    public class ManufacturerDO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
